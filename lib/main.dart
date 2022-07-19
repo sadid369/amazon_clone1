96053850e83f606ab0ce2c50e766e08f11e50b3b
Sadid <sadid.jones@gmail.com>
2022-07-19 20:51:57 +0600

    @19/07/2022 @ 3:24

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:amazon_clone1/constants/global_variables.dart';
 import 'package:amazon_clone1/features/auth/screens/auth_screen.dart';
+import 'package:amazon_clone1/features/auth/services/auth_service.dart';
+import 'package:amazon_clone1/features/home/screens/home_screen.dart';
 import 'package:amazon_clone1/providers/user_provider.dart';
 import 'package:amazon_clone1/router.dart';
 import 'package:flutter/material.dart';
@@ -13,10 +15,22 @@ void main() {
   ], child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
+  AuthServices authServices = AuthServices();
+  @override
+  void initState() {
+    super.initState();
+    authServices.getUserUser(context);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -33,6 +47,8 @@ class MyApp extends StatelessWidget {
               )),
         ),
         onGenerateRoute: (settings) => generateRoute(settings),
-        home: const AuthScreen());
+        home: Provider.of<UserProvider>(context).user.token.isNotEmpty
+            ? const HomeScreen()
+            : const AuthScreen());
   }
 }
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,6 +3,8 @@ const User = require("../models/user");
 const bcryptjs = require("bcryptjs");
 const authRouter = express.Router();
 const jwt = require("jsonwebtoken");
+const { json } = require("express");
+const auth = require("../middlewares/auth");
 // SIGN UP ROUTE
 authRouter.post("/api/signup", async (req, res) => {
   try {
@@ -45,5 +47,30 @@ authRouter.post("/api/signin", async (req, res) => {
   } catch (e) {
     res.status(500).json({ error: e.message });
   }
+});
+
+authRouter.post("/tokenIsValid", async (req, res) => {
+  try {
+    const token = req.header("x-auth-token");
+    if (!token) {
+      return res.json(false);
+    }
+    const verified = jwt.verify(token, "passwordKey");
+    if (!verified) {
+      return res.json(false);
+    }
+    const user = await User.findById(verified.id);
+    if (!user) {
+      return res.json(false);
+    }
+    res.json(true);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+//get user data
+authRouter.get("/", auth, async (req, res) => {
+  const user = await User.findById(req.user);
+  res.json({ ...user._doc, token: req.token });
 });
 module.exports = authRouter;
