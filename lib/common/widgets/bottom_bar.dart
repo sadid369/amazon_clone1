3838ca4a77f2f1730ee557a242bb3651ef83b03a
Sadid <sadid.jones@gmail.com>
2022-07-23 21:37:46 +0600

    @23/07/2022 @3:43

--- lib/common/widgets/bottom_bar.dart
+++ lib/common/widgets/bottom_bar.dart
@@ -1,4 +1,7 @@
 import 'package:amazon_clone1/constants/global_variables.dart';
+import 'package:amazon_clone1/features/account/screens/account_screen.dart';
+import 'package:amazon_clone1/features/home/screens/home_screen.dart';
+import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
 
 class BottomBar extends StatefulWidget {
@@ -13,15 +16,32 @@ class _BottomBarState extends State<BottomBar> {
   int _page = 0;
   double bottomBarWidth = 42;
   double bottomBarBorderWidth = 5;
+  List<Widget> pages = [
+    const HomeScreen(),
+    const AccountScreen(),
+    const Center(
+      child: Text(
+        "Cart Page",
+      ),
+    ),
+  ];
+  void updatePage(int page) {
+    setState(() {
+      _page = page;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: pages[_page],
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _page,
         selectedItemColor: GlobalVariables.selectedNavBarColor,
         unselectedItemColor: GlobalVariables.unselectedNavBarColor,
         backgroundColor: GlobalVariables.backgroundColor,
         iconSize: 28,
+        onTap: updatePage,
         items: [
           //HOME
           BottomNavigationBarItem(
@@ -73,7 +93,14 @@ class _BottomBarState extends State<BottomBar> {
                   ),
                 ),
               ),
-              child: const Icon(Icons.home_outlined),
+              child: Badge(
+                elevation: 0,
+                badgeContent: const Text("2"),
+                badgeColor: Colors.white,
+                child: const Icon(
+                  Icons.shopping_cart_outlined,
+                ),
+              ),
             ),
             label: "",
           ),
