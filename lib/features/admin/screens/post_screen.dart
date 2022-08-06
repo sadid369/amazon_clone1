97d2ddc02b99aa8ecafc1e8ba38a7061846fde0a
Sadid <sadid.jones@gmail.com>
2022-08-06 19:42:16 +0600

    @6/8/2022 @6:03

--- lib/features/admin/screens/post_screen.dart
+++ lib/features/admin/screens/post_screen.dart
@@ -1,4 +1,7 @@
+import 'package:amazon_clone1/constants/loader.dart';
 import 'package:amazon_clone1/features/admin/screens/add_product_screen.dart';
+import 'package:amazon_clone1/features/admin/services/admin_services.dart';
+import 'package:amazon_clone1/models/product.dart';
 import 'package:flutter/material.dart';
 
 class PostScreen extends StatefulWidget {
@@ -9,22 +12,39 @@ class PostScreen extends StatefulWidget {
 }
 
 class _PostScreenState extends State<PostScreen> {
+  List<Product>? products;
+  final AdminServices adminServices = AdminServices();
   void navigateToAddProduct() {
     Navigator.pushNamed(context, AddProductScreen.routeName);
   }
 
   @override
+  void initState() {
+    super.initState();
+    fetchAllProducts();
+  }
+
+  fetchAllProducts() async {
+    products = await adminServices.fetchAllProducts(context);
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: const Center(
-        child: Text("Products"),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: navigateToAddProduct,
-        child: const Icon(Icons.add),
-        tooltip: "Add A Product",
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-    );
+    print(products);
+    return products == null
+        ? const Loader()
+        : Scaffold(
+            body: const Center(
+              child: Text("Products"),
+            ),
+            floatingActionButton: FloatingActionButton(
+              onPressed: navigateToAddProduct,
+              child: const Icon(Icons.add),
+              tooltip: "Add A Product",
+            ),
+            floatingActionButtonLocation:
+                FloatingActionButtonLocation.centerFloat,
+          );
   }
 }
