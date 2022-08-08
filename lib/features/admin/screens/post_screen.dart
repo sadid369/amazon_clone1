729b42dcd0d181b817bb7652bd9d0bf95370e417
Sadid <sadid.jones@gmail.com>
2022-08-08 21:16:33 +0600

    @8/8/2022 @ 6:16

--- lib/features/admin/screens/post_screen.dart
+++ lib/features/admin/screens/post_screen.dart
@@ -1,4 +1,5 @@
 import 'package:amazon_clone1/constants/loader.dart';
+import 'package:amazon_clone1/features/account/widgets/single_product.dart';
 import 'package:amazon_clone1/features/admin/screens/add_product_screen.dart';
 import 'package:amazon_clone1/features/admin/services/admin_services.dart';
 import 'package:amazon_clone1/models/product.dart';
@@ -29,15 +30,58 @@ class _PostScreenState extends State<PostScreen> {
     setState(() {});
   }
 
+  void deleteProduct(Product product, int index) {
+    adminServices.deleteProduct(
+        context: context,
+        product: product,
+        onSuccess: () {
+          products!.removeAt(index);
+          setState(() {});
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     print(products);
     return products == null
         ? const Loader()
         : Scaffold(
-            body: const Center(
-              child: Text("Products"),
-            ),
+            body: GridView.builder(
+                itemCount: products!.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                ),
+                itemBuilder: (context, index) {
+                  final productData = products![index];
+                  return Column(
+                    children: [
+                      SizedBox(
+                        height: 140,
+                        child: SingleProduct(image: productData.images[0]),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Expanded(
+                            child: Text(
+                              productData.name,
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 2,
+                            ),
+                          ),
+                          IconButton(
+                            onPressed: () {
+                              deleteProduct(productData, index);
+                            },
+                            icon: const Icon(
+                              Icons.delete_outline,
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  );
+                }),
             floatingActionButton: FloatingActionButton(
               onPressed: navigateToAddProduct,
               child: const Icon(Icons.add),
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -32,4 +32,17 @@ adminRouter.get("/admin/get-products", admin, async (req, res) => {
   }
 });
 
+// Delete the Product
+
+adminRouter.post("/admin/delete-product", admin, async (req, res) => {
+  try {
+    const { id } = req.body;
+    let product = await Product.findByIdAndDelete(id);
+
+    res.json(product);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
 module.exports = adminRouter;
